daaf82d670216c3456653b5d50dd4b59c2d46e84
jessicafuung <jessica-fcc@hotmail.com>
2022-10-04 09:48:03 +0000

    forgot to save lol

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "pgr301-2021-terraform-state"
-    key    = "glennbech/demo.state"
+    key    = "chfu003/demo.state"
     region = "eu-north-1"
   }
 }
